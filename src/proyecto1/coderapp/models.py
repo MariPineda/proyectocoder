298c6f0019f38536b0bccbae39cd8d1077bd25c2
Mariana Pineda <marianapineda83@hotmail.com>
2023-12-14 17:07:41 -0300

    estaticos

--- src/proyecto1/coderapp/models.py
+++ src/proyecto1/coderapp/models.py
@@ -5,3 +5,6 @@ class Profesor(models.Model):
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
 
+    def __str__(self):
+        return f"{self.nombre}, {self.apellido}"
+
