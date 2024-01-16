631550b364df701835ceadaa590863f595744eb9
Mariana Pineda <marianapineda83@hotmail.com>
2024-01-16 12:20:20 -0300

    agregado

--- src/proyecto1/coderapp/models.py
+++ src/proyecto1/coderapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=20)
@@ -34,5 +35,12 @@ class Entregable(models.Model):
 
     def __str__(self):
         return f"{self.nombre} --- {self.entregado}"
+    
+class Avatar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='avatares', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.image}" 
 
 
